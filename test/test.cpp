780a2ba3d9c7158d828cc756728d955772a541ac
aaqibsb <aaqib.barodawala@gmail.com>
2023-10-23 18:16:25 -0400

    Task 1.7 Test cases for IK methods

--- test/test.cpp
+++ test/test.cpp
@@ -20,5 +20,12 @@ TEST(FK_Test, test_EF_position_2) {
 }
 
 TEST(IK_Test, test_JA_position_1) {
-
+  Eigen::Matrix4d transormationMatrix;
+  const a3c::JointAngles currentAngles = {{0, 0, 0, 0, 0, 0}};
+  const a3c::JointAngles targetAngles = {{0, 0, 0, 0, 0, 0}};
+  auto ik = a3c::InverseKinematics(transormationMatrix);
+  auto trajectory = ik.ik(currentAngles, targetAngles);
+  a3c::JointAngles expectedJA = {{0, 0, -90, 0, 0, 0}};
+  std::vector<a3c::JointAngles> expectedTraj={expectedJA};
+  EXPECT_EQ(trajectory, expectedTraj);
 }
