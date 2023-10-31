8596d26dd1eb6650bbf31cab708214f1a21d45b3
Jerry Pittman <jpittma1@umd.edu>
2023-10-31 12:24:50 -0400

    Task 1.2: added Jacobian Matrix
    Converted Python code of jacobian matrix into C++

--- libs/IK.cpp
+++ libs/IK.cpp
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <iostream>
 
+#include "Eigen/src/Core/Matrix.h"
 #include "include/InverseKinematics.hpp"
 
 namespace a3c {
@@ -30,6 +31,52 @@ InverseKinematics::InverseKinematics(const Eigen::Matrix4d& FM) noexcept {}
  */
 std::vector<JointAngles> InverseKinematics::ik(
     const JointAngles& currentAngles, const JointAngles& targetAngles) {
+  
+  const int size = 6;
+  MatrixXd J(size, size);
+  J.row(0) = -0.23000000000000001*sin(theta1)*sin(theta2) + 0.16350000000000001*sin(theta1)*sin(theta4)*cos(theta2 + theta3) - 0.16600000000000001*sin(theta1)*sin(theta5)*cos(theta4)*cos(theta2 + theta3) - 0.16600000000000001*sin(theta1)*sin(theta2 + theta3)*cos(theta5) - 0.23000000000000001*sin(theta1)*sin(theta2 + theta3) - 0.16600000000000001*sin(theta4)*sin(theta5)*cos(theta1) - 0.16350000000000001*cos(theta1)*cos(theta4) + 0.00050000000000000001*cos(theta1), 
+    0.16350000000000001*sin(theta4)*sin(theta2 + theta3)*cos(theta1) - 0.16600000000000001*sin(theta5)*sin(theta2 + theta3)*cos(theta1)*cos(theta4) + 0.23000000000000001*cos(theta1)*cos(theta2) + 0.16600000000000001*cos(theta1)*cos(theta5)*cos(theta2 + theta3) + 0.23000000000000001*cos(theta1)*cos(theta2 + theta3), 
+    0.16350000000000001*sin(theta4)*sin(theta2 + theta3)*cos(theta1) - 0.16600000000000001*sin(theta5)*sin(theta2 + theta3)*cos(theta1)*cos(theta4) + 0.16600000000000001*cos(theta1)*cos(theta5)*cos(theta2 + theta3) + 0.23000000000000001*cos(theta1)*cos(theta2 + theta3), 
+    0.16350000000000001*sin(theta1)*sin(theta4) - 0.16600000000000001*sin(theta1)*sin(theta5)*cos(theta4) - 0.16600000000000001*sin(theta4)*sin(theta5)*cos(theta1)*cos(theta2 + theta3) - 0.16350000000000001*cos(theta1)*cos(theta4)*cos(theta2 + theta3), 
+    -0.16600000000000001*sin(theta1)*sin(theta4)*cos(theta5) - 0.16600000000000001*sin(theta5)*sin(theta2 + theta3)*cos(theta1) + 0.16600000000000001*cos(theta1)*cos(theta4)*cos(theta5)*cos(theta2 + theta3), 
+    0;
+
+  J.row(1) = -0.16600000000000001*sin(theta1)*sin(theta4)*sin(theta5) - 0.16350000000000001*sin(theta1)*cos(theta4) + 0.00050000000000000001*sin(theta1) + 0.23000000000000001*sin(theta2)*cos(theta1) - 0.16350000000000001*sin(theta4)*cos(theta1)*cos(theta2 + theta3) + 0.16600000000000001*sin(theta5)*cos(theta1)*cos(theta4)*cos(theta2 + theta3) + 0.16600000000000001*sin(theta2 + theta3)*cos(theta1)*cos(theta5) + 0.23000000000000001*sin(theta2 + theta3)*cos(theta1),
+    0.16350000000000001*sin(theta1)*sin(theta4)*sin(theta2 + theta3) - 0.16600000000000001*sin(theta1)*sin(theta5)*sin(theta2 + theta3)*cos(theta4) + 0.23000000000000001*sin(theta1)*cos(theta2) + 0.16600000000000001*sin(theta1)*cos(theta5)*cos(theta2 + theta3) + 0.23000000000000001*sin(theta1)*cos(theta2 + theta3),
+    0.16350000000000001*sin(theta1)*sin(theta4)*sin(theta2 + theta3) - 0.16600000000000001*sin(theta1)*sin(theta5)*sin(theta2 + theta3)*cos(theta4) + 0.16600000000000001*sin(theta1)*cos(theta5)*cos(theta2 + theta3) + 0.23000000000000001*sin(theta1)*cos(theta2 + theta3),
+    -0.16600000000000001*sin(theta1)*sin(theta4)*sin(theta5)*cos(theta2 + theta3) - 0.16350000000000001*sin(theta1)*cos(theta4)*cos(theta2 + theta3) - 0.16350000000000001*sin(theta4)*cos(theta1) + 0.16600000000000001*sin(theta5)*cos(theta1)*cos(theta4),
+    -0.16600000000000001*sin(theta1)*sin(theta5)*sin(theta2 + theta3) + 0.16600000000000001*sin(theta1)*cos(theta4)*cos(theta5)*cos(theta2 + theta3) + 0.16600000000000001*sin(theta4)*cos(theta1)*cos(theta5);
+    0;
+
+  J.row(2) = 0,
+     -0.23000000000000001*sin(theta2) + 0.16350000000000001*sin(theta4)*cos(theta2 + theta3) - 0.16600000000000001*sin(theta5)*cos(theta4)*cos(theta2 + theta3) - 0.16600000000000001*sin(theta2 + theta3)*cos(theta5) - 0.23000000000000001*sin(theta2 + theta3),
+     0.16350000000000001*sin(theta4)*cos(theta2 + theta3) - 0.16600000000000001*sin(theta5)*cos(theta4)*cos(theta2 + theta3) - 0.16600000000000001*sin(theta2 + theta3)*cos(theta5) - 0.23000000000000001*sin(theta2 + theta3),
+     0.16600000000000001*sin(theta4)*sin(theta5)*sin(theta2 + theta3) + 0.16350000000000001*sin(theta2 + theta3)*cos(theta4),
+     -0.16600000000000001*sin(theta5)*cos(theta2 + theta3) - 0.16600000000000001*sin(theta2 + theta3)*cos(theta4)*cos(theta5),
+     0;
+
+
+  J.row(3) = 0,
+    -sin(theta1),
+    -sin(theta1),
+    sin(theta2)*cos(theta1)*cos(theta3) + sin(theta3)*cos(theta1)*cos(theta2),
+    -(-sin(theta2)*sin(theta3)*cos(theta1) + cos(theta1)*cos(theta2)*cos(theta3))*sin(theta4) - sin(theta1)*cos(theta4),
+    -(sin(theta1)*sin(theta4) - cos(theta1)*cos(theta4)*cos(theta2 + theta3))*sin(theta5) + sin(theta2 + theta3)*cos(theta1)*cos(theta5);
+
+  J.row(4) = 0,
+    cos(theta1),
+    cos(theta1),
+    sin(theta1)*sin(theta2)*cos(theta3) + sin(theta1)*sin(theta3)*cos(theta2),
+    -(-sin(theta1)*sin(theta2)*sin(theta3) + sin(theta1)*cos(theta2)*cos(theta3))*sin(theta4) + cos(theta1)*cos(theta4),
+    (sin(theta1)*cos(theta4)*cos(theta2 + theta3) + sin(theta4)*cos(theta1))*sin(theta5) + sin(theta1)*sin(theta2 + theta3)*cos(theta5);
+  
+  J.row(5) = 1,
+    0,
+    0,
+    -sin(theta2)*sin(theta3) + cos(theta2)*cos(theta3),
+    -(-sin(theta2)*cos(theta3) - sin(theta3)*cos(theta2))*sin(theta4),
+    -sin(theta5)*sin(theta2 + theta3)*cos(theta4) + cos(theta5)*cos(theta2 + theta3);
+
   // TODO(aaqibsb): Stuff.
   return {};
 }
